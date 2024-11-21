e8d3372801b892f900eb69bd55944b1feab9ce02
ljcalle <ljcalleb@gmail.com>
2024-11-21 13:17:17 +0100

    sin mensaje

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlantillaComponent } from "./plantilla/plantilla.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterOutlet, PlantillaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
